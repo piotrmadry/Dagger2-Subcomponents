addcec45ed4d924f489fd1ae9edabf75295bdaf4
Piotr Mądry <piotr@AppUnites-MacBook-Pro.local>
2017-10-27 09:09:25 +0200

    improve application class

--- app/src/main/java/com/example/piotr/subcomponentsexample/App.kt
+++ app/src/main/java/com/example/piotr/subcomponentsexample/App.kt
@@ -1,30 +1,11 @@
 package com.example.piotr.subcomponentsexample
 
-import android.app.Activity
-import android.app.Application
-import com.example.piotr.subcomponentsexample.components.AppComponent
 import com.example.piotr.subcomponentsexample.components.DaggerAppComponent
 import dagger.android.AndroidInjector
-import dagger.android.DispatchingAndroidInjector
-import dagger.android.HasActivityInjector
-import javax.inject.Inject
+import dagger.android.DaggerApplication
 
-class App : HasActivityInjector, Application() {
-
-    @Inject lateinit var injector: DispatchingAndroidInjector<Activity>
-
-    override fun activityInjector(): AndroidInjector<Activity> {
-        return injector
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-        initializeAppComponent().inject(this)
-    }
-
-    private fun initializeAppComponent(): AppComponent {
-        return DaggerAppComponent.builder()
-                .application(this)
-                .build()
+class App : DaggerApplication() {
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+        return DaggerAppComponent.builder().create(this)
     }
 }
--- app/src/main/java/com/example/piotr/subcomponentsexample/components/DaggerComponents.kt
+++ app/src/main/java/com/example/piotr/subcomponentsexample/components/DaggerComponents.kt
@@ -3,29 +3,20 @@ package com.example.piotr.subcomponentsexample.components
 import com.example.piotr.subcomponentsexample.App
 import com.example.piotr.subcomponentsexample.modules.ActivityBuildersModule
 import com.example.piotr.subcomponentsexample.modules.AppModule
-import dagger.BindsInstance
 import dagger.Component
+import dagger.android.AndroidInjector
 import dagger.android.support.AndroidSupportInjectionModule
 import javax.inject.Singleton
 
 
 @Singleton
-@dagger.Component(
+@Component(
         modules = arrayOf(
                 AppModule::class,
                 AndroidSupportInjectionModule::class,
                 ActivityBuildersModule::class
         ))
-interface AppComponent {
-
+interface AppComponent : AndroidInjector<App> {
     @Component.Builder
-    interface Builder {
-
-        @BindsInstance
-        fun application(app: App): Builder
-
-        fun build(): AppComponent
-    }
-
-    fun inject(app: App)
+    abstract class Builder : AndroidInjector.Builder<App>()
 }
